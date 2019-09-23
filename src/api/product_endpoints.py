b040cdb600974b926d0026bc318f43122c5542a5
asappscott <scott@asapp.com>
2019-09-23 16:32:29 -0400

    remove from cart/fix qty of existing items

--- src/api/product_endpoints.py
+++ src/api/product_endpoints.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, reqparse
 from src.api import db
 from src.api.helpers import is_logged_in
 
+
 class ProductInfo(Resource):
     def get(self, username, product_name):
         # get info about product quantity and price
@@ -18,11 +19,13 @@ class ProductInfo(Resource):
 
             return 'Product "{}" does not exist.', 400
 
+
 class ProductsInfo(Resource):
     def get(self, username):
         if not is_logged_in(db, username):
             return 'User must be logged-in to perform this action', 401
         return db.products.all(), 200
+
 
 class AddProductToCart(Resource):
     def post(self, username, product_name):
@@ -38,16 +41,24 @@ class AddProductToCart(Resource):
         results_from_inventory = db.search(db.products, query=(db.query.product_name == product_name))
         if results_from_inventory:
             product = results_from_inventory[0]  # making assumption we aren't making dupe products, just for now
+            results_from_cart = db.search(db.cart, query=(db.query.product_name == product_name))
+            if results_from_cart:
+                existing_qty = results_from_cart[0].get('product_qty')
+                needed_qty = existing_qty + needed_qty
+
             if product.get('product_qty') >= needed_qty:
                 # we will deduct the qty from the db at checkout time
-                item_for_cart = {'product_name': str(product_name), 'product_qty': int(needed_qty), 'cart_owner': str(username)}
-
-                db.insert(table=db.cart, data_dict=item_for_cart)
+                if results_from_cart:
+                    db.update(table=db.cart, update={'product_qty': needed_qty}, query=(db.query.product_name == product_name))
+                else:
+                    item_for_cart = {'product_name': str(product_name), 'product_qty': int(needed_qty), 'cart_owner': str(username)}
+                    db.insert(table=db.cart, data_dict=item_for_cart)
                 return 'QTY "{}" of product "{}" added to cart successfully'.format(needed_qty, product_name), 200
             else:
                 return 'Unable to request QTY "{}" for product "{}": insufficient inventory'.format(needed_qty, product_name), 400
 
-        return 'Product "{}" does not exist.', 400
+        return 'Product "{}" does not exist.'.format(product_name), 400
+
 
 class CartInfo(Resource):
     def get(self, username):
@@ -57,6 +68,20 @@ class CartInfo(Resource):
         cart = db.search(db.cart, query=(db.query.cart_owner == username))
         if cart:
             return cart
+
+
+class RemoveProductFromCart(Resource):
+    def post(self, username, product_name):
+        if not is_logged_in(db, username):
+            return 'User must be logged-in to perform this action', 401
+
+        results_from_cart = db.search(db.cart, query=(db.query.product_name == product_name))
+        if not results_from_cart:
+            return 'Product "{}" does not exist in cart.'.format(product_name), 400
+        else:
+            db.remove(db.cart, ((db.query.cart_owner == username) & (db.query.product_name == product_name)))
+            return 'Product "{}" successfully removed from cart.'.format(product_name), 200
+
 
 class CheckoutCart(Resource):
     def get(self, username):
