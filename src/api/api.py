def94fb338de7d9bddc9015d618c1b0cc4bf6649
asappscott <scott@asapp.com>
2019-09-24 15:40:11 -0400

    working host for flask

--- src/api/api.py
+++ src/api/api.py
@@ -40,4 +40,4 @@ if __name__ == '__main__':
     for product in default_inventory:
         db_session.insert(db_session.products, product)
 
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
