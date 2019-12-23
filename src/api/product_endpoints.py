008ad03bd124773147e7bdf808e370fe6f02dbc6
Eduardo Garcia <egarciachaurero@asapp.com>
2019-12-23 13:41:15 -0300

    Linting db, schema and product

--- src/api/product_endpoints.py
+++ src/api/product_endpoints.py
@@ -15,7 +15,9 @@ class ProductInfo(Resource):
         if not product_name:
             return db.products.all(), 200
         else:
-            product = db.search(db.products, query=(db.query.product_name == product_name))
+            product = db.search(db.products, query=(
+                db.query.product_name == product_name)
+            )
             if product:
                 return product[0], 200
 
@@ -38,26 +40,44 @@ class AddProductToCart(Resource):
         parser = reqparse.RequestParser()
         parser.add_argument('quantity')
         args = parser.parse_args()
-        needed_qty = int(args['quantity'])
+        req_qty = int(args['quantity'])
 
-        results_from_inventory = db.search(db.products, query=(db.query.product_name == product_name))
+        results_from_inventory = db.search(db.products, query=(
+                db.query.product_name == product_name)
+            )
         if results_from_inventory:
-            product = results_from_inventory[0]  # making assumption we aren't making dupe products, just for now
-            results_from_cart = db.search(db.cart, query=((db.query.cart_owner == username) & (db.query.product_name == product_name)))
+            # making assumption we aren't making dupe products, just for now
+            product = results_from_inventory[0]
+            results_from_cart = db.search(db.cart, query=(
+                (db.query.cart_owner == username) &
+                (db.query.product_name == product_name))
+                )
             if results_from_cart:
                 existing_qty = results_from_cart[0].get('product_qty')
-                needed_qty = existing_qty + needed_qty
+                req_qty = existing_qty + req_qty
 
-            if product.get('product_qty') >= needed_qty:
+            if product.get('product_qty') >= req_qty:
                 # we will deduct the qty from the db at checkout time
                 if results_from_cart:
-                    db.update(table=db.cart, update={'product_qty': needed_qty}, query=((db.query.cart_owner == username) & (db.query.product_name == product_name)))
+                    db.update(table=db.cart, update={'product_qty': req_qty},
+                              query=(
+                                (db.query.cart_owner == username) &
+                                (db.query.product_name == product_name)
+                              ))
                 else:
-                    item_for_cart = {'product_name': str(product_name), 'product_qty': int(needed_qty), 'cart_owner': str(username)}
+                    item_for_cart = {
+                        'product_name': str(product_name),
+                        'product_qty': int(req_qty),
+                        'cart_owner': str(username)}
                     db.insert(table=db.cart, data_dict=item_for_cart)
-                return 'QTY "{}" of product "{}" added to cart successfully'.format(needed_qty, product_name), 200
+                return 'QTY "{}" of product "{}" added to cart'.format(
+                    req_qty, product_name
+                ), 200
             else:
-                return 'Unable to request QTY "{}" for product "{}": insufficient inventory'.format(needed_qty, product_name), 400
+                return 'Unable to request QTY "{}" for product \
+                    "{}": insufficient inventory'.format(
+                        req_qty, product_name
+                    ), 400
 
         return 'Product "{}" does not exist.'.format(product_name), 400
 
@@ -77,12 +97,18 @@ class RemoveProductFromCart(Resource):
         if not is_logged_in(db, username):
             return 'User must be logged-in to perform this action', 401
 
-        results_from_cart = db.search(db.cart, query=((db.query.cart_owner == username) & (db.query.product_name == product_name)))
+        results_from_cart = db.search(db.cart, query=(
+                (db.query.cart_owner == username) &
+                (db.query.product_name == product_name))
+            )
         if not results_from_cart:
-            return 'Product "{}" does not exist in cart.'.format(product_name), 400
+            return 'Product "{}" not in cart.'.format(product_name), 400
         else:
-            db.remove(db.cart, ((db.query.cart_owner == username) & (db.query.product_name == product_name)))
-            return 'Product "{}" successfully removed from cart.'.format(product_name), 200
+            db.remove(db.cart, (
+                (db.query.cart_owner == username) &
+                (db.query.product_name == product_name)
+            ))
+            return 'Product "{}" removed from cart.'.format(product_name), 200
 
 
 class CheckoutCart(Resource):
@@ -94,13 +120,18 @@ class CheckoutCart(Resource):
         for cart_product in cart:
             cart_product_name = cart_product.get('product_name')
             cart_product_qty = cart_product.get('product_qty')
-            inventory_product = db.search(db.products, query=(db.query.product_name == cart_product_name))[0]
-            if inventory_product.get('product_qty') >= cart_product_qty:
-                new_qty = inventory_product.get('product_qty') - cart_product_qty
+            inventory_prdct = db.search(db.products, query=(
+                    db.query.product_name == cart_product_name
+                ))[0]
+            if inventory_prdct.get('product_qty') >= cart_product_qty:
+                new_qty = inventory_prdct.get('product_qty') - cart_product_qty
                 db.update(table=db.products, update={'product_qty': new_qty},
                           query=(db.query.product_name == cart_product_name))
             else:
-                return 'Unable to request QTY "{}" for product "{}": insufficient inventory'.format(cart_product_qty, cart_product_name), 400
+                return 'Unable to request QTY "{}" for \
+                    product "{}": insufficient inventory'.format(
+                        cart_product_qty, cart_product_name
+                    ), 400
 
         db.remove(db.cart, (db.query.cart_owner == username))
         return 'Checkout successful! Thank you for shopping with us.', 200
